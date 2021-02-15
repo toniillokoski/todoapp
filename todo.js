e14ef58837c6cfbe825f72c52f9937a9b95661ae
Toni Illokoski <toni.illokoski@gmail.com>
2021-02-15 22:30:06 +0200

    Added some basics for css

--- todo.js
+++ todo.js
@@ -1,22 +1,15 @@
 function newtodo()
 {
     //Muuttuja listan luomiseen
-    var li = document.createElement("li");
+    var listi = document.createElement("li");
     //Muuttuja jolla haetaan inputin arvo (käyttäjän syöttämä)
     var inputti = document.getElementById("addtodo").value;
-    //Muuttuja jolla annetaan listalle child arvo
+    //Muuttuja jolla annetaan listalle child arvoksi inputin arvo
     var todovalue = document.createTextNode(inputti);
     //Lisää käyttäjän syöte listan arvoksi
-    li.appendChild(todovalue)
+    listi.appendChild(todovalue)
     //Tulosta uusi lista
-    document.getElementById("UL").appendChild(li);
+    document.getElementById("UL").appendChild(listi);
 
-    document.getElementById("UL").value = "";
-  
-    /*for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }*/
+
 }
